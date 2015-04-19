5d603aa2824b795a1350c10c5af41d9829428745
Devin Rader <devin@twilio.com>
2015-04-19 13:27:17 -0400

    Added test to validate proper deserialization of new Metadata list JSON format

---
+++ Twilio.Api.Pcl.Tests/Resources/FakeMetaDataListResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twilio.Api.Pcl.Tests
+{
+    public class FakeMetaDataListResult : MetadataListBase
+    {
+        public List<Stuff> Stuffs { get; set; }
+    }
+
+    public class Stuff {
+        public string Name {get;set;}
+    }
+}
